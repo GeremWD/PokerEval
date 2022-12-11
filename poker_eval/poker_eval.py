0024fb74cd0ed4404f23b10eca1f4ad85e0a4a12
= <gerem.hutin@gmail.com>
2022-12-11 16:00:14 +0100

    example inside poker_eval.py

--- poker_eval/poker_eval.py
+++ poker_eval/poker_eval.py
@@ -174,3 +174,30 @@ class Evaluator:
         checker = self.get_checker(pocket, board)
         odds = self.check_odds(pocket, board, n_samples)
         return rank, checker, odds
+
+
+def print_result_preflop(odds):
+    print(f"Odds : {odds*100:.2f} %\n")
+
+def print_result_after_flop(rank, checker, odds):
+    rank_str = evaluator.rank_to_str(rank)
+    print(f"Rank : {rank_str} ({rank}),  Checker : {checker:.2f},  Odds : {odds*100:.2f} %\n")
+
+
+if __name__ == '__main__':
+    evaluator = Evaluator()
+
+    pocket = 'ad8h'
+    flop = '4cth2d'
+    turn = 'as'
+    river = 'td'
+
+    _, _, odds = evaluator.full_evaluation(pocket, "")
+    print("Preflop : ")
+    print_result_preflop(odds)
+    print("Flop")
+    print_result_after_flop(*evaluator.full_evaluation(pocket, flop, n_samples=20000))
+    print("Turn")
+    print_result_after_flop(*evaluator.full_evaluation(pocket, flop + turn))
+    print("River")
+    print_result_after_flop(*evaluator.full_evaluation(pocket, flop + turn + river))
